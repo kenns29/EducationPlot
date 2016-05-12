30459ea99c77bcd4708d9966a515763bacc77995
hxwang <hxwang@asu.edu>
2016-05-12 14:00:04 -0700

    text label on slider

--- public/draw.js
+++ public/draw.js
@@ -22,18 +22,52 @@ function slider(data){
 		}
 	});
 
-	d3.select('#slider-ticks').selectAll('span')
+	var s_width = $('#slider').width();
+	var t_width = $('#slider-ticks').width();
+	var t_height = $('#slider-ticks').height();
+	var t_margin = {left:(t_width - s_width) / 2, right:(t_width - s_width)/2, top:2, bottom:1};
+
+	var marks = d3.select('#slider-ticks').append('svg')
+	.attr('width', t_width+t_margin.left+t_margin.right)
+	.attr('height', t_height+t_margin.top + t_margin.bottom)
+	.append('g')
+	.attr('transform', 'translate(' + [t_margin.left, t_margin.top] + ')')
+	.selectAll('g')
 	.data(d3.range(yearRange[0], yearRange[1] + 1, 1))
-	.enter().append('span')
-	.style('position', 'absolute')
-	.style('left', function(d, i){
-		var p = 100 / (yearRange[1] - yearRange[0]);
-		return i * p + '%';
+	.enter()
+	.append('g');
+
+	var ticks = marks.append('line')
+	.attr('x1', function(d, i){
+		var p = s_width / (yearRange[1] - yearRange[0]);
+		return i * p;
 	})
-	.html(function(d, i){
+	.attr('y1', function(){
+		return 2;
+	})
+	.attr('x2', function(d, i){
+		var p = s_width / (yearRange[1] - yearRange[0]);
+		return i * p;
+	})
+	.attr('y2', function(){
+		return 10;
+	}).attr('stroke', 'black').attr('stroke-width', 1);
+
+
+	var texts = marks.append('text')
+	.attr('x', function(d, i){
+		var p = s_width / (yearRange[1] - yearRange[0]);
+		return i * p;
+	})
+	.attr('y', function(){
+		return 12;
+	})
+	.attr('dominant-baseline', 'hanging')
+	.attr('text-anchor', 'middle')
+	.attr('fill', 'black')
+	.text(function(d){
 		return d;
 	});
-
 	function getYearRange(data){
 		var min = new Date().getFullYear();
 		var max = 0;
