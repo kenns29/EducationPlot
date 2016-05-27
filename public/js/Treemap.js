fbef0b624df35bd24bc9190f80acafe90c3d0db0
hxwang <hxwang@asu.edu>
2016-05-27 16:28:03 -0700

    treemap

--- public/js/Treemap.js
+++ public/js/Treemap.js
@@ -8,6 +8,11 @@ function Treemap(){
 		width = $(graph_container).width();
 		height = $(graph_container).height();
 
+		if(width === 0 || height === 0){
+			width = $('#canvas').width();
+			height = $('#canvas').height();
+		}
+		
 		svg = d3.select(graph_container).append('svg')
 		.attr('width', width).attr('height', height);
 
@@ -20,23 +25,34 @@ function Treemap(){
 	};
 
 	this.update = function(){
-		// var dat = data.map(function(d){
-		// 	return {
-		// 		'name' : d.InstName,
-		// 		'id' : d.Inst
-		// 		'value' : d.Pell[state.year]
-		// 	};
-		// })
+		
+		var color_scale = d3.scale.quantize().domain([0, 100]).range(['white','#d4b9da','#c994c7','#df65b0','#e7298a','#ce1256','#91003f']);
+		var dat = {
+			'name' : 'root', 
+			'children' : data.filter(function(d, i){
+				return d.Pell[state.year] > 0 && d.GradRate[state.year] > 0;
+			}).map(function(d){
+				return {
+					'name' : d.InstName,
+					'id' : d.UnitID,
+					'GradRate' : d.GradRate[state.year],
+					'size' : d.Pell[state.year]
+				};
+			})
+		};
+		
 
 		var treemap = d3.layout.treemap()
 		.size([W, H]).sticky(true).value(function(d){
-			console.log('d', d);
-			return d.Pell[state.year];
+			return d.size;
 		});
 
+		var treemap_nodes = treemap.nodes(dat);
+		console.log('treemap nodes', treemap_nodes);
+
 		var nodes = treemap_g.selectAll('.treemap-nodes')
-		.data(treemap.nodes(data), function(d){
-			return d.UnitID;
+		.data(treemap_nodes, function(d){
+			return d.id;
 		});
 
 		var nodesEnter = nodes.enter().append('g')
@@ -54,7 +70,9 @@ function Treemap(){
 		nodesUpdate.selectAll('rect')
 		.attr('x', 0).attr('y', 0)
 		.attr('width', function(d){return d.dx;}).attr('height', function(d){return d.dy;})
-		.attr('stroke-width', 1).attr('stroke', 'black');
+		.attr('stroke-width', 1).attr('stroke', 'black').attr('fill', function(d){
+			return color_scale(d.GradRate);
+		});
 
 		return this;
 	};
